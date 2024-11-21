67b050b2b30056118f0718dd006dbc21e4c00086
Maysam Habbash <126085708+may1001sam@users.noreply.github.com>
2024-11-21 21:37:39 +0200

    final

--- Task3/server.py
+++ Task3/server.py
@@ -1,8 +1,10 @@
 from socket import *
 from questions import *
+import random
+import time
 
 # Port on which the server listens
-serverPort = 6250
+serverPort = 8000
 hostName = gethostname()
 serverIP = gethostbyname(hostName)
 
@@ -24,16 +26,38 @@ def broadcast_players(name):
   for client in activeClients:
     serverSocket.sendto(message.encode(), client)
 
+# Function to broadcast questions
+def broadcast_question(question, count):
+  message = f"Question {count}: {question}\n Your answer (or type 'exit' to quit):"
+  for client in activeClients:
+    serverSocket.sendto(message.encode(), client)
+
+# Function to select a number of questions
 def selectQuestions():
-  
+  numberOfQuestion = 3
+  selectedQuestions = random.sample(questions, numberOfQuestion)
+  count = 0
+  # Broadcast questions to playeres
+  for i in selectedQuestions:
+    count += 1
+    if count > 1:
+      time.sleep(60)
+    print(f"Question {count}: {i}\n")
+    broadcast_question(i, count)
+    ###############################################################################################
+    ################# You can start working from here, accepting answers from players ##################
+    ###################################################################################################
 
 # Function to start the game
 def startGame():
-  print("Starting the Trivia Game  Round in 90 seconds!")
+  print("Starting the Trivia Game Round in 90 seconds!")
+  time.sleep(90)
+
   # broadcasting a welcome message to notify clients that the round is about to start
   startingMessage = "Starting the Trivia Game  Round in 90 seconds! Get ready!"
   for client in activeClients:
     serverSocket.sendto(startingMessage.encode(), client)
+    selectQuestions()
 
 while (True):
   # Check minimum number of players
@@ -47,8 +71,9 @@ while (True):
   if clientIP not in activeClients:
     activeClients.append(clientIP)
 
+    print()
     print(f"{clientName.decode()} joined the game from {clientIP}")
-    welcomingMessage = f"Registered with Trivia Game server at IP {serverIP}, Port {serverPort}\n Wainting for the game to start . . ."
+    welcomingMessage = f"Registered with Trivia Game server at IP {serverIP}, Port {serverPort}\n Waiting for the game to start . . ."
 
     serverSocket.sendto(welcomingMessage.encode(), clientIP)
     broadcast_players(clientName.decode())
